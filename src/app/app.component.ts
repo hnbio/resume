12db59390d509778d91fee1445e3770e9a98a86e
jackeybiao <hanbiao4079@qq.com>
2022-06-12 11:40:39 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import resume_md from './resume_md';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+
+  resume = resume_md
 }
---
+++ src/app/resume_md.ts
@@ -0,0 +1,109 @@
+
+
+export default {
+  name: "韩彪",
+  phone: "18112891269",
+  email: "hanbiao4079@qq.com",
+  sex: "男",
+  college: "蚌埠医学院",
+  profession: "信息管理与信息系统",
+  degree: "本科",
+  graduation: "2017年7月",
+  undergoing: [
+    {
+      school: '蚌埠医学院',
+      start_time: "2013年9月",
+      end_time: "2017年7月",
+      profession: "信息管理与信息系统",
+    }
+  ],
+  ability: [
+    "1. 熟练掌握HTML、CSS、javascript,以及前端常用的工具。",
+    "2. 熟练熟悉了解各种前端框架(angular,react,vue)并使用",
+    "3. 了解各种大前端开发部署(react-native, electron, 小程序等)",
+    "4. 了解nodejs, 常用数据库(mysql,mongodb)等",
+  ],
+  work_undergoing: [
+    {
+      company: "频率探索",
+      time: "2021.10-2022.06",
+      content: "参与负责ai设备卫士相关系统及工具开发工作"
+    },
+    {
+      company: "壹零贰肆加信息技术有限公司",
+      time: "2020.09-2021.10",
+      content: "参与负责加拓宝客销平台部分功能模块开发维护"
+    },
+    {
+      company: "常州深兰人工智能研究院",
+      time: "2019.09-2020.09",
+      content: "参与熊猫智行开发维护，负责广告运营, 智行小程序等系统开发开发"
+    },
+    {
+      company: "创云一智科技有限公司",
+      time: "2018.09-2019.09",
+      content: "负责业财一体化管理平台前端开发维护上线"
+    },
+    {
+      company: "艾特沃品牌策划",
+      time: "2017.07-2018.09",
+      content: "负责公司内部门户网站开发维护上线工作"
+    },
+  ],
+  projects: [
+    {
+      source: "频率探索",
+      title: "ai设备卫士",
+      synopsis: "电机类设备故障监控检测管理系统及对应app开发",
+      technology: "angular、tailwindcss, antd, react-native等",
+      address: "http://freqx.com/",
+      docmentUrl: "http://freqx.com/",
+      time: "2021.10-2022.06",
+      content: "负责项目整体开发维护, 并在职期间开发必要的通用业务组件, 指令、管道等、以满足产品快速迭代需求。并编写自动化node脚本处理常用的打包部署",
+    },
+    {
+      source: "频率探索",
+      title: "智能质检平台",
+      synopsis: "基于electron的桌面版设备数据采集展示系统",
+      time: "2022.02-04",
+      technology: "vue3、element-ui, echarts、electron、nodejs、express、mongodb、mq等",
+      content: "负责客户端项目整体开发, 使用蓝牙传感器采集设备运转数据，并通过对应算法模型给出对应设备运行结果（前期主要是出厂合格检查及设备故障检查用）。客户端主要采集指令发出及计算结果展示等内容",
+    }
+  ],
+  projects1: [
+    {
+      source: "壹零贰肆加信息技术有限公司",
+      title: "加拓宝客销平台, 企业微信h5, 基于uni-app的智能外呼小程序等",
+      synopsis: "基于react全家桶开发的客销平台(客户线索、crm等)",
+      technology: "react、、hooks、redux, antd, TypeScript等",
+      time: "2020.10-2021.09",
+      address: "http://www.jtbai.com/user/login",
+      docmentUrl: "http://jtbai.com/hc/zh-CN/",
+      content: "负责crm模块开发迭代,及邮件触达、消息管理等模块功能开发。独自负责企业微信端,及基于uni-app的小程序转接外呼(避免拨号拉黑用)及拨号记录管理功能, 在司期间积极推动前端项目组代码规范及review, 前端自动化工具在前端项目中应用等",
+    },
+    {
+      source: "常州深兰人工智能研究院",
+      title: "熊猫智行平台、广告运营平台、平安智能猪只投保系统、教育系统、熊猫智行小程序等",
+      synopsis: "基于原生微信小程序api打造, 主要用户公交车试乘预约",
+      technology: "react, antd, 微信小程序等",
+      time: "2019.09-2019.10",
+      content: "在职期间参与并负责熊猫智行管理端开发维护工作，并负责广告运营平台、投保系统搭建开发上线, 并参与教育web app端开发及对应服务端node转发服务开发。负责小程序开发维护及对应后台管理功能开发",
+    },
+    {
+      source: "创云一智科技有限公司",
+      title: "业财一体化管理系统",
+      synopsis: "基于vue全家桶, 搭建的业财一体化管理平台",
+      technology: "vue2, element-ui",
+      time: "2018.10-2019.09",
+      content: "负责项目前端框架搭建及整体功能开发(内网项目), 主要功能是集团下属公司提交季度财务预算,然后分公司集团进行提交审核汇总展示。并结合其他资金申请审核批复功能等",
+    },
+    {
+      source: "艾特沃品牌策划",
+      title: "各种基于wordpress的门户网站",
+      synopsis: "独自负责基于wordpress博客系统开发门户网站",
+      technology: "wordpress, 少量php模板语法, 及mysql数据库",
+      time: "2017.07-2018.09",
+      content: "负责公司内部门户网站制作并部署交付（目前上线中）",
+    },
+  ],
+}
